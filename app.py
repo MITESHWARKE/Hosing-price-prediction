4dcbb7d3f1ff40b69675493592e16bc0456af47b
MITESHWARKE <miteshwarke@gmail.com>
2022-11-07 15:38:00 +0530

    Updated CI CD

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "The CI CD pipelining has been established"
+    return "[Updated] The CI CD pipelining has been established"
 
 if __name__=="__main__":
     app.run(debug=True)
