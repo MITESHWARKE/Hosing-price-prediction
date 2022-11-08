190efdd69a6974af3953e3e53fc549020f97b3bd
MITESHWARKE <miteshwarke@gmail.com>
2022-11-08 11:21:39 +0530

    Setup.py update

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -7,7 +7,6 @@ PROJECT_NAME= "housing-predictor"
 VERSION="0.0.1"
 AUTHOR="Mitesh"
 DESCRIPTION="This is the first November betch fsds project"
-PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:  
@@ -20,14 +19,14 @@ def get_requirements_list()->List[str]:
     """
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 
 )
