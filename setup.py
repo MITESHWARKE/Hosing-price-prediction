d95a0b8fb06a241aac7dbc097e6ef3d94342b304
MITESHWARKE <miteshwarke@gmail.com>
2022-12-27 18:02:56 +0530

    final update

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ from typing import List
 #Declaring variables for setup functions
 PROJECT_NAME= "housing-predictor"
 VERSION="0.0.1"
-AUTHOR="Mitesh"
-DESCRIPTION="This is the first November betch fsds project"
+AUTHOR="Mitesh Warke"
+DESCRIPTION="This is the first November batch fsds project"
 
 REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements_list()->List[str]:  
     """
@@ -20,7 +22,11 @@ def get_requirements_list()->List[str]:
     """
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .")
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 
 setup(
